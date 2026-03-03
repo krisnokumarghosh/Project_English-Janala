ae2892765a92d163f5426f75c02223b7de41744d
krisnokumarghosh <krishnokumarghosh112@gmail.com>
2026-03-03 19:47:14 +0600

    added words

--- script/index.js
+++ script/index.js
@@ -5,6 +5,43 @@ const loadLessons = () =>{
     .then((json) => displayLesson(json.data));
 }
 
+const loadLevelWord = (id) =>{
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data)) 
+}
+
+
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById('word-container');
+    wordContainer.innerHTML = "";
+
+    words.forEach((word) =>{
+        console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+        
+            <div class="bg-white rounded-md text-center px-[20px] py-[56px] space-y-4">
+          <div class="space-y-[20px]">
+            <h2 class="font-bold text-[20px]">${word.word}</h2>
+            <p class="font-medium ">meaning/pronounciation</p>
+            <div class="font-bangla font-semibold text-[20px] text-[#18181B]">"${word.pronunciation} / ${word.meaning}"</div>
+          </div>
+            <div class="flex justify-between items-center">
+                <button class="btn btn-soft btn-info"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn btn-soft btn-info"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+
+        `
+
+        wordContainer.append(card);
+        
+    })
+    
+}
+
 const displayLesson = (lessons) =>{
     console.log(lessons);
 
@@ -14,7 +51,7 @@ const displayLesson = (lessons) =>{
     for(let lesson of lessons){
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-         <button class="btn btn-outline btn-primary">
+         <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
          <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
          </button>
         
